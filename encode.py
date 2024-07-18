385e9c31a229d5273b0900352b1b8f7e6778efd9
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2024-07-18 15:39:06 +0200

    Update encode.py

--- encode.py
+++ encode.py
@@ -2,29 +2,26 @@ import cv2
 
 video = cv2.VideoCapture(r'./video.mp4')
 
+def is_close_to_black(pixel, threshold=20):
+    # Check if any of the RGB values are below the threshold
+    return all(component < threshold for component in pixel)
 
-COLORS = []
-with open('video.txt', 'w') as file:
-    file.flush()
+with open('video.txt', 'w') as file:  # Use 'w' for writing text data
     currentframe = 0
-    while(True):
-        # if currentframe > 1: break
-        ret,frame = video.read()
+    while True:
+        ret, frame = video.read()
 
-        if ret:
-            print('Captured ' + str(currentframe))
-            
-            framelist = frame.tolist()
-            
-            for x in framelist:
-                file.write(','.join(['-'.join(map(str, sublist)) for sublist in x]) + '\n')
-                
-            
-            currentframe += 1
-        else: 
+        if not ret:
             break
-    
-    file.close()
+        
+        for y in range(frame.shape[0]):
+            filtered_pixels = [pixel for pixel in frame[y] if not is_close_to_black(pixel)]
+            if filtered_pixels:
+                line = ','.join('-'.join(map(str, pixel)) for pixel in frame[y])
+            else:
+                line = '0'
+            file.write(line + '\n')
+        
+        currentframe += 1
 
-video.release() 
-cv2.destroyAllWindows()
+video.release()
